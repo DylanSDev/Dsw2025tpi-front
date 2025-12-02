00a8da2041299c3216174cd24334f7d160bc989e
Atilio <atilio.l.vergara@gmail.com>
2025-12-02 08:33:07 -0300

    Se reparo el error de visualizacion de los productos

--- src/modules/products/services/list.js
+++ src/modules/products/services/list.js
@@ -1,7 +1,6 @@
 import { instance } from '../../shared/api/axiosInstance';
 
-//Lista productos para la vista de administración (usa el endpoint /api/products/admin).
-export const getProducts = async (search = null, status = null, pageNumber = 1, pageSize = 20 ) => {
+export const getProducts = async (search = '', status = "All", pageNumber = 1, pageSize = 10 ) => {
   const queryString = new URLSearchParams({
     search,
     status,
@@ -14,20 +13,14 @@ export const getProducts = async (search = null, status = null, pageNumber = 1, 
   return { data: response.data, error: null };
 };
 
-/**
- * Obtiene productos para la vista pública (usa el endpoint /api/products).
- * Siempre filtra por status 'enabled' (habilitados) para el cliente [REQUISITO].
- */
-export const getClientProducts = async (search = null, pageNumber = 1, pageSize = 20) => {
+export const getClientProducts = async (search = '', pageNumber = 1, pageSize = 10) => {
   const queryString = new URLSearchParams({
     search,
-    status: 'enabled', // Solo habilitados para el cliente
     pageNumber,
     pageSize,
   });
 
-  // Endpoint público: /api/products
-  const response = await instance.get(`api/products?${queryString}`); 
+  const response = await instance.get(`api/products?${queryString}`);
 
   return { data: response.data, error: null };
 };
